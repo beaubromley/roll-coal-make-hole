80465f8e5deb927e851c863d4b5086d175873834
beaubromley <beaubromley@gmail.com>
2025-11-30 14:10:23 -0600

    Bump version to v1.0.1

--- js/config/version.js
+++ js/config/version.js
@@ -1,7 +1,7 @@
 const GAME_VERSION = {
     MAJOR: 1,
     MINOR: 0,
-    PATCH: 0,
+    PATCH: 1, // Changed from 0 to 1
     toString() {
         return `v${this.MAJOR}.${this.MINOR}.${this.PATCH}`;
     }
